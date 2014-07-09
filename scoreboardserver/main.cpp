ecc8af187e887d2086055f9e881dc4fe88e772d6
Brian Richardson <skinny@knowhere.net>
2014-07-09 16:21:11 -0700

    Initial commit.

---
+++ scoreboardserver/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  scoreboardserver
+//
+//  Created by Brian Richardson on 7/9/14.
+//  Copyright (c) 2014 churchofrobotron. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
